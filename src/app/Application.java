04ea833e2b832e6d46d241c57069e61af14447e5
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2017-07-05 11:14:24 -0300

    Add database

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,10 +1,19 @@
 package app;
+import java.sql.SQLException;
+
+import framework.*;
 
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("App main");
+		try {
+			Database.insertUser("Paulo Markes", "123456");
+			Database.insertUser("Ze", "78910");
+			
+			Database.viewTableUser();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/app/Student.java
+++ src/app/Student.java
@@ -1,29 +1,32 @@
 package app;
+import java.sql.SQLException;
+
 import framework.*;
 public class Student extends User{
 
 	@Override
-	public void create(String arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
+	public void create(String name, String registration) {
+		Database.insertUser(name, registration);
 	}
 
 	@Override
-	public void delete() {
-		// TODO Auto-generated method stub
-		
+	public void delete(int idUser) {
+		Database.deleteUser(idUser);
 	}
 
 	@Override
-	public boolean update() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(int idUser, String name, String registration) {
+		Database.updateUser(idUser, name, registration);
+		return true;
 	}
 
 	@Override
 	public void visualize() {
-		// TODO Auto-generated method stub
-		
+		try {
+			Database.viewTableUser();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
