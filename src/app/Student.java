aefde4aa3d9b1742b22f535fc6faefad6229ddb3
Lucas Brilhante <lbrilhante82@gmail.com>
2017-07-05 00:00:03 -0300

    Adding reference to jar file and creating Student that extends User

---
+++ src/app/Student.java
@@ -0,0 +1,29 @@
+package app;
+import framework.*;
+public class Student extends User{
+
+	@Override
+	public void create(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void visualize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
